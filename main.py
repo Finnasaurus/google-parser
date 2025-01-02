b1ed024f58775f04c9541d556851a75ba99b013e
Finnasaurus <bentenqin@gmail.com>
2025-01-02 16:29:10 +0800

    uncommenting stuff

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+import json
 import serpapi
-from dotenv import load_dotenv
+
 import httpx
 from bs4 import BeautifulSoup
 import re
@@ -13,13 +14,50 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 SERP_API_KEY = os.environ.get("SERP_API_KEY")
+COUNTRY_LIST = 'google-countries.json'
 
 class serpScrape:
     def __init__(self):
         pass
 
-    def search(self, search="ducks", engine="duckduckgo",\
-               loc="Australia"):
+    def __parseJson(self, filepath: str=None) -> list:
+        try:
+            data = json.load(open(filepath, 'r'))
+            return data
+        except FileNotFoundError:
+            print(f"File not found: {filepath}")
+            return None
+        except json.JSONDecodeError as e:
+            print(f"Invalid JSON format: {e}")
+            return None
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None       
+        
+    def displayCountry(self, countryQuery: str=None) -> str:
+        ''' Displays countries with their respective country codes.
+        `countryQuery` does nothing at the moment.'''
+        
+        count = 0
+        countries = self.__parseJson(COUNTRY_LIST)
+        output = "List of countries:\n"
+
+        # Find the longest country name for padding
+        maxLength = max(len(country['country_name']) for country in countries)
+        
+        for country in countries:
+            output += (
+                f"{country['country_name']:<{maxLength}} : "  # Left align with dynamic padding
+                f"{country['country_code']}\n"
+            )
+            count += 1
+        
+        output += f"\nOutput {count} lines of country."
+        
+        return output.format()
+    
+
+    def search(self, search: str=None, engine: str=None, loc="Australia"):
         '''Available search engines: `bing`, `google`, `duckduckgo`, `yahoo`.\n
         Search query defaults to `ducks` if nothing is input\n
         Search engine defaults to `duckduckgo` if nothing is input'''
@@ -68,7 +106,7 @@ class serpScrape:
         return emails
     
 scrape = serpScrape()
-results = scrape.search(search="cleaning distributor supplier",engine='google')
+results = scrape.search(search="-",engine='google')
 
 results_de_duplicate = []
 for ele in results["organic_results"]:
